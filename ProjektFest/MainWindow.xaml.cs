7bc3bc75ee618009521ffefbcb183f1e9fa84062
Matejhko <matej.malek7@gmail.com>
2024-04-17 17:08:56 +0200

    Seznam-pijace.json file se ob prvi uporabi ustvari, potem pa bere iz bin/debug/windows neke

--- ProjektFest/MainWindow.xaml.cs
+++ ProjektFest/MainWindow.xaml.cs
@@ -32,11 +32,35 @@ namespace ProjektFest
         {
             InitializeComponent();
             Main.Content = new StartPage(this);
-            string json = File.ReadAllText("seznam_pijac.json");
 
+            string json = "";
 
-
-            seznam_pijac = JsonSerializer.Deserialize<List<Pijaca>>(json);
+            //1. najprej pridobi podatke iz tistega jsona
+            List<Pijaca> seznam_pijace = Utilities.StalnaPijaca();
+            //2.Pridobi lokacijo bin/debug računalnika
+            string filepath = AppDomain.CurrentDomain.BaseDirectory.ToString()+"seznam_pijac.json";
+            //3. Preveri če obstaja datoteka seznam_pijace.json
+            if (File.Exists(filepath))
+            {
+                //Datoteka že obstaja, samo preberemo jo
+                json = File.ReadAllText("seznam_pijac.json");
+                seznam_pijac = JsonSerializer.Deserialize<List<Pijaca>>(json);
+            }
+            else
+            {
+                //Datoteka ne obstaja, ustvarimo datoteko na tej lokaciji in jo napolnimo s podatki iz list<pijaca>, nato jo preberemo
+                json = JsonSerializer.Serialize(seznam_pijace);
+                try
+                {
+                    File.WriteAllText(filepath, json);
+                    json = File.ReadAllText(filepath);
+                    seznam_pijace = JsonSerializer.Deserialize<List<Pijaca>>(json);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Pri branju datoteke seznam_pijace.json je prišlo do napake: {ex.Message}");
+                }
+            }
         }
     }
 }
